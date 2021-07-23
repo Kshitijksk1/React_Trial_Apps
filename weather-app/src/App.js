e85e09a6a14fa3a663c0bec258b30daded4c9d6a
Kshitij Khaladkar <kkhaladkar55@gmail.com>
2021-07-22 19:47:28 -0500

    Second Commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,18 +1,67 @@
-//import logo from './logo.svg';
-import SearchBar from './Components/searchBar';
-import './App.css';
-import React from 'react';
+import React from "react";
+import "./App.css";
 
-//const FARENHEIT = "farehnheit";
-function App(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <SearchBar/>
-      </header>
-    </div>
-  );
+import SearchBar from "./Components/searchBar";
+import CurrentWeather from "./Components/current-weather";
+import Forecast from "./Components/forecast";
+
+import * as Api from "./API/open-weather.api";
+
+const FARENHEIT = "farehnheit";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "",
+      metric: FARENHEIT,
+      hourlyForecast: [],
+      current: "",
+    };
+
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.updateTemperature = this.updateTemperature.bind(this);
+
+    this.updateTemperature();
+  }
+
+  handleLocationChange(location) {
+    this.setState({ location });
+  }
+
+  async updateTemperature() {
+    const weatherRes = await Api.getWeatherBasedOnLocation(this.state.location);
+    const forecastRes = await Api.getForecast(
+      weatherRes.coord.lat,
+      weatherRes.coord.lon
+    );
+
+    this.setState({
+      current: forecastRes.current,
+      metric: FARENHEIT,
+      hourlyForecast: forecastRes.hourly,
+    });
+  }
+
+  render() {
+    const location = this.state.location;
+    const hourlyForecast = this.state.hourlyForecast;
+    const current = this.state.current;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SearchBar
+            searchValue={location}
+            onSearchChange={this.handleLocationChange}
+            onFormSubmit={this.updateTemperature}
+          />
+
+          {current && <CurrentWeather current={current} />}
+          {hourlyForecast.length > 0 && <Forecast forecast={hourlyForecast} />}
+        </header>
+      </div>
+    );
+  }
 }
-
 
 export default App;
